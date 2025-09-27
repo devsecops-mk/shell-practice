ce2e2c602da741b59f9b8baead467b7324d5f31f
Mohan Krishna <mohankrishna.gudimetla@gmail.com>
2025-09-27 20:59:51 +0530

    02-conversation script created

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: Hi Suresh, How are you ??"
+echo "Suresh :: Hello Ramesh, Iam fine, How are you ??"
+echo "Ramesh :: Iam learning Devops at joindevops "
+echo "Suresh :: Thats a good idea !!! "
