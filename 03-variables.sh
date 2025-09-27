9efa3df708a63194a8ab64cc9015a7582889e254
Mohan Krishna <mohankrishna.gudimetla@gmail.com>
2025-09-27 21:49:47 +0530

    02-conversation script created

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=John
+PERSON2=Syman
+
+echo "$PERSON1 :: Hi $PERSON2, How are you ??"
+echo "$PERSON2 :: Hello $PERSON1, Iam fine, How are you ??"
+echo "$PERSON1 :: Iam learning Devops at joindevops "
+echo "$PERSON2 :: Thats a good idea !!! "
