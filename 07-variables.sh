8d1d5b2a3a902edd1f85fa5899fd03fc09fc7015
Mohan Krishna <mohankrishna.gudimetla@gmail.com>
2025-09-28 15:58:12 +0530

    date variable

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+present_date=$(date +"%Y-%m-%d : %H-%M")
 
+echo "present date and time is : $present_date "
 START_TIME=$(date +%s)
 
 sleep 10 
@@ -8,7 +10,8 @@ END_TIME=$(date +%s)
 
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 
-echo "date +%c "
 echo "Script executed in : $TOTAL_TIME  seconds"
+current_date=$(date +"%Y-%m-%d : %H-%M")
 
+echo "Current date and time is : $current_date"
 
