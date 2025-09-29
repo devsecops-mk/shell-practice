cdc07678b1a67e08cbaf89524fb5b1fd15175522
Mohan Krishna <mohankrishna.gudimetla@gmail.com>
2025-09-29 09:02:10 +0530

    If condition apply

---
+++ 14-functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR :: Please run the script with root previleges ..."
+    exit 1 # failure is other than 0
+fi
+
+VAIDATE() { # functions received inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then
+        echo "ERROR ::  Installing $2 is failure"
+        exit 1
+    else
+        echo "Installing $2 is success"
+    fi
+}
+
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+dnf install python3 -y
+VALIDATE $? "Python3"
+
+dnf install zip -y
+VALIDATE $? "Zip"
